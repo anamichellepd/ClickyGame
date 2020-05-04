b964b7ef6c758e7bf9cee9008656220d99a39183
Ana Pineda <anamichellepd@gmail.com>
2020-05-04 00:39:20 -0700

    starting all over again because I had too many irrelevant things and have learned more

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,18 @@
 import React, { Component } from "react";
-import Topbar from "./components/Topbar";
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
 import friends from "./friends.json";
-
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends,
+    friends
   };
 
-  shuffleFriends = (friends) => {
-    let i =friends.length -;
-    for(;i>0;i--){
-      const j = MAth.floor (Math.random()*(i+1));
-      const temp = friends[i];
-      friends[i] = friends [j];
-      friends[j]= temp;
-    }
-    return friends
-  };
-
-  removeFriend = (id) => {
+  removeFriend = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter((friend) => friend.id !== id);
+    const friends = this.state.friends.filter(friend => friend.id !== id);
     // Set this.state.friends equal to the new friends array
     this.setState({ friends });
   };
@@ -32,22 +20,20 @@ class App extends Component {
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <Container fluid>
-        <Topbar fluid></Topbar>
-        <Wrapper>
-          {this.state.friends.map((friend) => (
-            <FriendCard
-              removeFriend={this.removeFriend}
-              id={friend.id}
-              key={friend.id}
-              name={friend.name}
-              image={friend.image}
-              occupation={friend.occupation}
-              location={friend.location}
-            />
-          ))}
-        </Wrapper>
-      </Container>
+      <Wrapper>
+        <Title>Friends List</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
--- my-app/src/components/FriendCard/index.js
+++ my-app/src/components/FriendCard/index.js
@@ -3,10 +3,26 @@ import "./style.css";
 
 function FriendCard(props) {
   return (
-    <div className="card" onClick={() => console.log("hiiiiii")}>
+    <div className="card">
       <div className="img-container">
         <img alt={props.name} src={props.image} />
       </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+          <li>
+            <strong>Occupation:</strong> {props.occupation}
+          </li>
+          <li>
+            <strong>Location:</strong> {props.location}
+          </li>
+        </ul>
+      </div>
+      <span onClick={() => props.removeFriend(props.id)} className="remove">
+        𝘅
+      </span>
     </div>
   );
 }
